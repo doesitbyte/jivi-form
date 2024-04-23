704a63aa481577c52d823ac5ed34e6a8d83d5454
Kaustubh <kaustubhsharma999@gmail.com>
2024-04-23 15:24:24 +0530

    improved html layout

--- src/jivi-form/components/Form.tsx
+++ src/jivi-form/components/Form.tsx
@@ -30,15 +30,17 @@ export default function Form() {
 
     const processForm: SubmitHandler<TFormDataSchema> = async data => {
         setLoading(true);
-        const res = await axios.post("https://dummyjson.com/http/200", data);
-        console.log(res);
-        if (res.status === 200) {
-            setStep(step => step + 1)
-            formMethods.reset();
-            setLoading(false);
-            return res
+        try {
+            const res = await axios.post("https://dummyjson.com/http/200", data);
+            if (res.status === 200) {
+                setStep(step => step + 1)
+                formMethods.reset();
+            } else {
+                throw new Error;
+            }
+        } catch {
+            alert("Something went wrong! Please try again.")
         }
-        alert("Something went wrong! Please try again.")
         setLoading(false);
     }
 
@@ -50,137 +52,132 @@ export default function Form() {
 
     return (
         <>
-            <main className='w-full sm:w-2/3 lg:w-1/2 min-h-full flex flex-col items-stretch justify-between my-8 mx-4'>
+            <main className='flex flex-col h-full p-4'>
                 {/* Form */}
-                <div className='basis-11/12'>
-                    <form className='flex flex-col min-w-full min-h-full' onSubmit={formMethods.handleSubmit(processForm)}>
-                        <div className='grow flex-col'>
-                            {step === 0 && (
-                                <motion.div
-                                    initial={{ y: '100%', opacity: 0 }}
-                                    animate={{ y: 0, opacity: 1 }}
-                                    transition={{ duration: 0.3, ease: 'easeInOut' }}
-                                >
-                                    <Details formMethods={formMethods} />
+                <div className='basis-10/12'>
+                    <form onSubmit={formMethods.handleSubmit(processForm)}>
+                        {step === 0 && (
+                            <motion.div
+                                initial={{ y: '100%', opacity: 0 }}
+                                animate={{ y: 0, opacity: 1 }}
+                                transition={{ duration: 0.3, ease: 'easeInOut' }}
+                            >
+                                <Details formMethods={formMethods} />
 
-                                </motion.div>
-                            )}
+                            </motion.div>
+                        )}
 
-                            {step === 1 && (
-                                <motion.div
-                                    initial={{ x: '100%', opacity: 0 }}
-                                    animate={{ x: 0, opacity: 1 }}
-                                    transition={{ duration: 0.3, ease: 'easeInOut' }}
-                                >
-                                    <WeightPicker formMethods={formMethods} />
-                                </motion.div>
-                            )}
+                        {step === 1 && (
+                            <motion.div
+                                initial={{ x: '100%', opacity: 0 }}
+                                animate={{ x: 0, opacity: 1 }}
+                                transition={{ duration: 0.3, ease: 'easeInOut' }}
+                            >
+                                <WeightPicker formMethods={formMethods} />
+                            </motion.div>
+                        )}
 
-                            {step === 2 && (
-                                <motion.div
-                                    initial={{ x: '100%', opacity: 0 }}
-                                    animate={{ x: 0, opacity: 1 }}
-                                    transition={{ duration: 0.3, ease: 'easeInOut' }}
-                                >
-                                    <AgePicker formMethods={formMethods} />
-                                </motion.div>
-                            )}
+                        {step === 2 && (
+                            <motion.div
+                                initial={{ x: '100%', opacity: 0 }}
+                                animate={{ x: 0, opacity: 1 }}
+                                transition={{ duration: 0.3, ease: 'easeInOut' }}
+                            >
+                                <AgePicker formMethods={formMethods} />
+                            </motion.div>
+                        )}
 
-                            {step === 3 && (
-                                <motion.div
-                                    initial={{ x: '100%', opacity: 0 }}
-                                    animate={{ x: 0, opacity: 1 }}
-                                    transition={{ duration: 0.3, ease: 'easeInOut' }}
-                                >
-                                    <Preview formMethods={formMethods} />
-                                </motion.div>
-                            )}
+                        {step === 3 && (
+                            <motion.div
+                                initial={{ x: '100%', opacity: 0 }}
+                                animate={{ x: 0, opacity: 1 }}
+                                transition={{ duration: 0.3, ease: 'easeInOut' }}
+                            >
+                                <Preview formMethods={formMethods} />
+                            </motion.div>
+                        )}
 
-                            {step === 4 && (
-                                <motion.div
-                                    initial={{ opacity: 0 }}
-                                    animate={{ opacity: 1 }}
-                                    transition={{ duration: 0.3, ease: 'easeInOut' }}
-                                >
-                                    <Success />
-                                </motion.div>
-                            )}
-                        </div>
+                        {step === 4 && (
+                            <motion.div
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                transition={{ duration: 0.3, ease: 'easeInOut' }}
+                            >
+                                <Success />
+                            </motion.div>
+                        )}
                     </form>
                 </div>
 
                 {/* Navigation */}
-                <nav className='basis-1/12'>
-                    <div className='mt-4'>
-                        {(step === 0 || step === 1 || step === 2)
-                            && (
-                                <motion.div
-                                    initial={{ y: '100%', opacity: 0 }}
-                                    animate={{ y: 0, opacity: 1 }}
-                                    transition={{ duration: 0.3, ease: 'easeInOut' }}
+                <nav className='basis-2/12'>
+                    {(step === 0 || step === 1 || step === 2)
+                        && (
+                            <motion.div
+                                initial={{ y: '100%', opacity: 0 }}
+                                animate={{ y: 0, opacity: 1 }}
+                                transition={{ duration: 0.3, ease: 'easeInOut' }}
+                            >
+                                <Button
+                                    type='default'
+                                    className={(formMethods.formState.isValid) ?
+                                        "w-full h-[70px] rounded-xl hover:bg-jiviBlue hover:opacity-90 bg-jiviBlue font-semibold text-white font-jivi text-md" :
+                                        "w-full h-[70px] rounded-xl hover:bg-jiviGray hover:opacity-90 bg-jiviGray font-semibold text-white font-jivi text-md"
+                                    }
+                                    onClick={next}
                                 >
-                                    <Button
-                                        type='default'
-                                        className={(formMethods.formState.isValid) ?
-                                            "w-full h-[70px] rounded-xl hover:bg-jiviBlue hover:opacity-90 bg-jiviBlue font-semibold text-white font-jivi text-md" :
-                                            "w-full h-[70px] rounded-xl hover:bg-jiviGray hover:opacity-90 bg-jiviGray font-semibold text-white font-jivi text-md"
-                                        }
-                                        onClick={next}
-                                    >
-                                        {"Next"}
-                                    </Button>
-                                </motion.div>
-                            )}
+                                    {"Next"}
+                                </Button>
+                            </motion.div>
+                        )}
 
-                        {step === 3
-                            && (
-                                <motion.div
-                                    initial={{ x: '100%', opacity: 0 }}
-                                    animate={{ x: 0, opacity: 1 }}
-                                    transition={{ duration: 0.3, ease: 'easeInOut' }}
-                                >
-                                    <div className='flex flex-row justify-between'>
-                                        <Button
-                                            type="text"
-                                            className='basis-1/2 mr-2 h-[70px] bg-jiviGray text-white font-jivi text-md font-semibold'
-                                            onClick={() => setStep(0)}
-                                            disabled={loading}
-                                        >
-                                            {"Edit"}
-                                        </Button>
-                                        <Button
-                                            type="primary"
-                                            className='basis-1/2 ml-2 h-[70px] bg-jiviBlue text-white font-jivi text-md font-semibold'
-                                            onClick={
-                                                formMethods.handleSubmit(processForm)
-                                            }
-                                            loading={loading}
-                                        >
-                                            {"Submit"}
-                                        </Button>
-                                    </div>
-                                </motion.div>
-                            )}
-
-                        {step === 4
-                            && (
-                                <motion.div
-                                    initial={{ y: '100%', opacity: 0 }}
-                                    animate={{ y: 0, opacity: 1 }}
-                                    transition={{ duration: 0.3, ease: 'easeInOut' }}
-                                >
+                    {step === 3
+                        && (
+                            <motion.div
+                                initial={{ x: '100%', opacity: 0 }}
+                                animate={{ x: 0, opacity: 1 }}
+                                transition={{ duration: 0.3, ease: 'easeInOut' }}
+                            >
+                                <div className='flex flex-row justify-between'>
                                     <Button
-                                        type='default'
-                                        className="w-full h-[70px] rounded-xl hover:bg-jiviBlue hover:opacity-90 bg-jiviBlue font-semibold text-white font-jivi text-md"
+                                        type="text"
+                                        className='basis-1/2 mr-2 h-[70px] bg-jiviGray text-white font-jivi text-md font-semibold'
                                         onClick={() => setStep(0)}
+                                        disabled={loading}
                                     >
-                                        {"Submit Again"}
+                                        {"Edit"}
                                     </Button>
-                                </motion.div>
-                            )}
-                    </div>
-                </nav>
+                                    <Button
+                                        type="primary"
+                                        className='basis-1/2 ml-2 h-[70px] bg-jiviBlue text-white font-jivi text-md font-semibold'
+                                        onClick={
+                                            formMethods.handleSubmit(processForm)
+                                        }
+                                        loading={loading}
+                                    >
+                                        {"Submit"}
+                                    </Button>
+                                </div>
+                            </motion.div>
+                        )}
 
+                    {step === 4
+                        && (
+                            <motion.div
+                                initial={{ y: '100%', opacity: 0 }}
+                                animate={{ y: 0, opacity: 1 }}
+                                transition={{ duration: 0.3, ease: 'easeInOut' }}
+                            >
+                                <Button
+                                    type='default'
+                                    className="w-full h-[70px] rounded-xl hover:bg-jiviBlue hover:opacity-90 bg-jiviBlue font-semibold text-white font-jivi text-md"
+                                    onClick={() => setStep(0)}
+                                >
+                                    {"Submit Again"}
+                                </Button>
+                            </motion.div>
+                        )}
+                </nav>
             </main>
         </>
     )
--- src/jivi-form/components/ui/JiviAgePicker.tsx
+++ src/jivi-form/components/ui/JiviAgePicker.tsx
@@ -26,7 +26,7 @@ const JiviAgePicker = (props: JiviAgePickerProps) => {
                         age: field.value.toString()
                     } : defaultAge} onChange={(value) => {
                         field.onChange(parseInt(value.age));
-                    }} wheelMode='natural' height={500} itemHeight={100}>
+                    }} wheelMode='natural' height={400} itemHeight={80}>
                         <Picker.Column name={"age"}>
                             {selections.age.map(option => (
                                 <Picker.Item key={option} value={option}>
@@ -34,8 +34,9 @@ const JiviAgePicker = (props: JiviAgePickerProps) => {
                                         <span
                                             className={
                                                 selected ?
-                                                    "cursor-pointer w-[128px] flex justify-center items-center rounded-[24px] leading-[100px] text-[72px] text-4xl font-bold bg-jiviBlue text-white border-solid border-8 border-blue-200/90" :
-                                                    `cursor-pointer text-jiviDarkGray leading-[136px] text-[${(80 - ((Math.abs(parseInt(field.value || defaultAge.age) - parseInt(option))) * 20)).toString() + "px"}] font-bold`
+                                                    "cursor-pointer w-[128px] flex justify-center items-center rounded-[24px] leading-[100px] text-[72px] font-bold bg-jiviBlue text-white border-solid border-8 border-blue-200/90" :
+                                                    // `cursor-pointer text-jiviDarkGray text-5xl font-bold`
+                                                    `cursor-pointer text-jiviDarkGray text-${(6 - Math.abs(parseInt(field.value || defaultAge.age) - parseInt(option))).toString() + "xl"} font-bold`
                                             }
                                         >
                                             {option}
